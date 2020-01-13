1aaaeae6d07fd305dfc0a4b9473d8a12ee35dc4c
Aibek-KZ <aibek.risbek@gmail.com>
2020-01-13 09:32:13 -0700

    added some changes

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "",
+        tags = "@store_manager",
         plugin = {"html:target/default-cucumber-reports",
                 "json:target/cucumber.json",
                   "rerun:target/rerun.txt"}
